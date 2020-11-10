f57dee987c4cb7081986c3f5bed62254dbe64529
lukewhyte <lukeawhyte@gmail.com>
2020-11-10 11:30:18 -0600

    Added private env variables for auth

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,11 +11,14 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const dotenvPlugin = require('cypress-dotenv');
 
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+	// `config` is the resolved Cypress config
+	config = dotenvPlugin(config)
+  return config
 }
