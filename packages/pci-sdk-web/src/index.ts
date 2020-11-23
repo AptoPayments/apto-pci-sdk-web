ea8b117ac11a6de7536f1bf33ab3ed2a2bb25369
IagoLast <iago.lastra@gmail.com>
2020-11-23 21:38:52 +0100

    feat: Better error messages

--- packages/pci-sdk-web/src/index.ts
+++ packages/pci-sdk-web/src/index.ts
@@ -1,5 +1,7 @@
 
 import { version } from '@apto-payments/pci-sdk-iframe';
+
+// This is required!
 export { version } from '@apto-payments/pci-sdk-iframe';
 
 export interface InitOptions {
@@ -17,7 +19,7 @@ export interface IAuthOptions {
 	environment: 'stg' | 'sbx' | 'prd';
 }
 
-export interface PCIStyle { }
+export interface PCIStyle { } // eslint-disable-line
 
 export interface Size {
 	width: string;
@@ -83,17 +85,20 @@ export function getIsDataVisible(): Promise<boolean> {
 }
 
 function _checkInitOptions(initOptions: InitOptions) {
+	if(!initOptions) {
+		throw new Error('Cannot call .init() with no options. At least auth parameters are required: http://docs.aptopayments.com/docs/pci-sdk-web#initialize-the-sdk');
+	}
 	if (!initOptions.auth.apiKey) {
-		throw new Error('You need to provide an API token to init the PCI SDK');
+		throw new Error('You need to provide the mobile API key init the PCI SDK: http://docs.aptopayments.com/docs/pci-sdk-web/#get-the-mobile-api-key');
 	}
 	if (!initOptions.auth.userToken) {
-		throw new Error('You need to provide an user token to init the PCI SDK');
+		throw new Error('You need to provide an user token to init the PCI SDK: http://docs.aptopayments.com/docs/pci-sdk-web/#optionsobject-properties');
 	}
 	if (!initOptions.auth.cardId) {
-		throw new Error('You need to provide a cardID to init the PCI SDK');
+		throw new Error('You need to provide a cardID to init the PCI SDK: http://docs.aptopayments.com/docs/pci-sdk-web/#optionsobject-properties');
 	}
 	if (!initOptions.auth.environment) {
-		throw new Error('You need to provide an environment to init the PCI SDK');
+		throw new Error('You need to provide an environment to init the PCI SDK. Try "sbx" if you are testing: http://docs.aptopayments.com/docs/pci-sdk-web/#optionsobject-properties');
 	}
 }
 
@@ -137,7 +142,10 @@ function _initIframe(authOptions: IAuthOptions, pciElement: HTMLElement | null =
 	});
 }
 
-function _sendMessage(data: object) {
+function _sendMessage(data: {type: string, theme?: any, style?: any }) {
+	if (!$aptoIframe) {
+		return console.error(`Cannot execute "${data.type}". It looks like the SDK is not initialised, run AptoPCISdk.init() first`);
+	}
 	return $aptoIframe.then(frame => {
 		const messageEvent = JSON.stringify(data);
 		frame.contentWindow?.postMessage(messageEvent, CORS_DOMAIN);
