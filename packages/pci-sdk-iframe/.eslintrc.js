fc8b9f14d92593b46d36c5fdcd99a37cb1ecf433
IagoLast <iago.lastra@gmail.com>
2020-11-25 08:43:41 +0100

    chore: Add prettier config

--- packages/pci-sdk-iframe/.eslintrc.js
+++ packages/pci-sdk-iframe/.eslintrc.js
@@ -1,15 +1,11 @@
 module.exports = {
-	extends: [
-		"react-app",
-		"typescript",
-		"typescript/react",
-	],
+	extends: ['react-app', 'typescript', 'typescript/react', 'prettier'],
 	ignorePatterns: [
 		// '**/*.stories.tsx',
 	],
 	overrides: [
 		{
-			files: ["**/*.ts?(x)"],
+			files: ['**/*.ts?(x)'],
 			rules: {
 				'@typescript-eslint/ban-types': 1,
 				'@typescript-eslint/camelcase': 0,
@@ -19,14 +15,9 @@ module.exports = {
 				'@typescript-eslint/no-explicit-any': 0,
 				'@typescript-eslint/prefer-as-const': 0, // TODO: Investigate this rule, maybe we want it
 				'import/no-anonymous-default-export': 0, // We export anonymous services.
-				'@typescript-eslint/semi': ["error"],
-				'indent': ['error', 'tab', { "SwitchCase": 1 }],
-				'quotes': ['error', 'single'],
 				'react/no-unescaped-entities': 0, // We disable this because aleja likes reading the characters
 				'react/prop-types': 0, // we use typescript iterfaces, we don't need this
-				'semi': 0,
-				'no-case-declarations': 0,
-			}
-		}
+			},
+		},
 	],
-}
+};
