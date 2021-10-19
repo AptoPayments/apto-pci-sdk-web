65d3297fa24692e1b015b6465a9e5545b809fd5e
Alejandra Arri <aleja@aptopayments.com>
2021-10-19 12:28:16 +0200

    chore: Rename StaticState into configOptions (#70)
    * Rename to configOptions
    
    Co-authored-by: IagoLast <iago.lastra@gmail.com>

--- packages/pci-sdk-iframe/src/hooks/useApplicationState.ts
+++ packages/pci-sdk-iframe/src/hooks/useApplicationState.ts
@@ -1,17 +1,17 @@
-import themes from 'pages/app/themes';
+import themes, { IThemeName } from 'pages/app/themes';
 import IApplicationState from 'types/IApplicationState';
-import { IStaticState } from 'types/IStaticState';
+import { IConfigOptions } from 'types/IConfigOptions';
 import usePureState from './usePureState';
 
-export default function useApplicationState(staticState: IStaticState) {
+export default function useApplicationState(configOptions: IConfigOptions) {
 	const { state, dispatch } = usePureState<IApplicationState>({
 		cvv: '•••',
 		exp: '••/••',
 		uiStatus: 'CARD_DATA_HIDDEN',
 		isLoading: false,
 		message: '',
-		pan: `•••• •••• •••• ${staticState.lastFour}`,
-		theme: themes[staticState.theme],
+		pan: `•••• •••• •••• ${configOptions.lastFour}`,
+		theme: themes[configOptions.theme as IThemeName],
 		verificationId: '', // Used to get the 2FA code
 		nextStep: '',
 	});
---
+++ packages/pci-sdk-iframe/src/hooks/useConfigOptions.ts
@@ -0,0 +1,38 @@
+import { IThemeName } from 'pages/app/themes';
+import { useState } from 'react';
+import { IConfigOptions } from 'types/IConfigOptions';
+
+export default function useConfigOptions() {
+	const urlParams = new URLSearchParams(window.location.search);
+
+	const [configOptions] = useState<IConfigOptions>(() => ({
+		cardId: (urlParams.get('cardId') as string) || '',
+		codePlaceholderMessage: (urlParams.get('codePlaceholderMessage') as string) || 'Enter the code',
+		enter2FAPrompt: (urlParams.get('enter2FAPrompt') as string) || 'Enter the code we sent you (numbers only).',
+		expiredMessage: (urlParams.get('expiredMessage') as string) || 'Process expired. Start again.',
+		failed2FAPrompt: (urlParams.get('failed2FAPrompt') as string) || 'Wrong code. Try again.',
+		isDebug: !!urlParams.get('debug'),
+		// This can be true false or undefined if we don't have info
+		isPCICompliant: _getIsPCICompliant(urlParams.get('isPCICompliant')),
+		labelCvv: (urlParams.get('labelCvv') as string) || 'Cvv',
+		labelExp: (urlParams.get('labelExp') as string) || 'Exp',
+		labelName: (urlParams.get('labelName') as string) || 'Name',
+		labelPan: (urlParams.get('labelPan') as string) || 'Card number',
+		lastFour: (urlParams.get('lastFour') as string) || '••••',
+		nameOnCard: (urlParams.get('nameOnCard') as string) || '',
+		pinPlaceholderMessage: (urlParams.get('pinPlaceholderMessage') as string) || 'Enter your new PIN',
+		pinUpdatedMessage: (urlParams.get('pinUpdatedMessage') as string) || 'Pin successfully updated',
+		theme: (urlParams.get('theme') as IThemeName) || 'light',
+		tooManyAttemptsMessage: (urlParams.get('tooManyAttemptsMessage') as string) || 'Too many attempts. Start again.',
+	}));
+
+	return configOptions;
+}
+
+function _getIsPCICompliant(value: string | null): boolean | undefined {
+	if (value === undefined || value === null) {
+		return undefined;
+	}
+
+	return value === 'true';
+}
