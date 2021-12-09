57633a8488d30f4f1da5120a36613673d0ee1396
Alejandra Arri <aleja@aptopayments.com>
2021-12-09 19:43:52 +0100

    feat: Update iframe to get the default value from the url (1201439445598524)

--- packages/pci-sdk-iframe/src/apiClient/errorMessage.parser.ts
+++ packages/pci-sdk-iframe/src/apiClient/errorMessage.parser.ts
@@ -7,7 +7,7 @@ function parse401(errorCode: number, serverMessage: string): string {
 		case 3035:
 			return 'The mobile API key you provided is invalid. Please set a valid API key: https://docs.aptopayments.com/docs/sdks/Web/pci_sdk_web/#get-the-mobile-api-key';
 		case 90262:
-			return 'Unexpected error in cardholder verification. Please restart verification process or contact APTO if problem persists.';
+			return 'Unexpected error in cardholder verification. Please restart verification process or contact Apto if problem persists.';
 		case 90000:
 		default:
 			/// If 90000, somehow a header property is missing. This shouldn't be possible. Information is in the message provided by API:
@@ -16,11 +16,11 @@ function parse401(errorCode: number, serverMessage: string): string {
 }
 
 function parse400(customMessage?: string): string {
-	return customMessage ? customMessage : 'Invalid request. Please try again or contact APTO';
+	return customMessage ? customMessage : 'Invalid request. Please try again or contact Apto.';
 }
 
 function parseUnknownError(): string {
-	return 'An unknown error happened. Please try again or contact APTO.';
+	return 'An unknown error happened. Please try again or contact Apto.';
 }
 
 export default {
--- packages/pci-sdk-iframe/src/types/IStaticState.d.ts
+++ packages/pci-sdk-iframe/src/types/IStaticState.d.ts
@@ -1,21 +1,29 @@
 import { IThemeName } from '../pages/app/themes';
 
 export interface IStaticState {
-	cardId: string;
-	codePlaceholderMessage: string;
-	enter2FAPrompt: string;
-	expiredMessage: string;
-	failed2FAPrompt: string;
-	isDebug: boolean;
-	isPCICompliant: boolean | undefined;
-	labelCvv: string;
-	labelExp: string;
-	labelName: string;
-	labelPan: string;
-	lastFour: string;
-	nameOnCard: string;
-	pinPlaceholderMessage: string;
-	pinUpdatedMessage: string;
-	theme: IThemeName;
-	tooManyAttemptsMessage: string;
+	card: {
+		cardId: string;
+		labelCvv: string;
+		labelExp: string;
+		labelName: string;
+		labelPan: string;
+		lastFour: string;
+		nameOnCard: string;
+		theme: IThemeName;
+	};
+	config: {
+		isDebug: boolean;
+		isPCICompliant: boolean | undefined;
+	};
+	labels: {
+		codePlaceholder: string;
+		pinPlaceholder: string;
+	};
+	messages: {
+		enter2FA: string;
+		expired2FA: string;
+		failed2FA: string;
+		tooManyAttempts: string;
+		pinUpdated: string;
+	};
 }
